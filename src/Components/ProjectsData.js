2b01159e3abccb6763483e3bb7f6f00171e156a7
Shashank Sharma <shashanksharma962727@gmail.com>
2026-01-28 21:34:10 +0530

    Add Projects

--- src/Components/ProjectsData.js
+++ src/Components/ProjectsData.js
@@ -1,4 +1,7 @@
 import clima from '../assets/Projects/Clima.JPG'
+import portfolio from '../assets/Projects/Portfolio.JPG'
+import world from '../assets/Projects/world-analysis.JPG'
+import soon from '../assets/Projects/soon.JPG'
 
 export const projectsData = [
     {
@@ -14,29 +17,29 @@ export const projectsData = [
     {
     id: 2,
     title: "Portfolio Website",
-    description: "Personal portfolio built with React and Tailwind CSS.",
-    image: "/images/portfolio.png",
-    tech: ["React", "Tailwind", "Context API"],
-    live: "https://yourportfolio.com",
-    github: "https://github.com/username/portfolio",
+    description: "Created a responsive personal portfolio site using React and deployed it on Vercel to showcase skills, projects, and contact info.",
+    image: portfolio,
+    tech: ["React", "Tailwind", "Context API","Emial.js", "Framer Motion"],
+    live: "https://portfolio-rose-psi-42.vercel.app/",
+    github: "https://github.com/ShashankSharma962727/Portfolio",
     },
-
+//React.js, Javascript(ES6), Tailwind css, API Integration (axios), React router, Framer Motion, 
     {
     id: 3,
-    title: "Portfolio Website",
-    description: "Personal portfolio built with React and Tailwind CSS.",
-    image: "/images/portfolio.png",
-    tech: ["React", "Tailwind", "Context API"],
-    live: "https://yourportfolio.com",
-    github: "https://github.com/username/portfolio",
+    title: "world intelligence platform",
+    description: "Built a React web application to display global intelligence data with a clean, responsive interface. Implemented reusable components, API integration, and seamless navigation. ",
+    image: world,
+    tech: ["React", "Tailwind", "Context API", "React router", "Framer Motion", "API (Axios)"],
+    live: "https://world-intelligence-platform.vercel.app/",
+    github: "https://github.com/ShashankSharma962727/World-Intelligence-Platform",
     },
 
     {
     id: 4,
-    title: "Portfolio Website",
-    description: "Personal portfolio built with React and Tailwind CSS.",
-    image: "/images/portfolio.png",
-    tech: ["React", "Tailwind", "Context API"],
+    title: "Coming Soon",
+    description: "Coming Soon",
+    image: soon,
+    tech: [],
     live: "https://yourportfolio.com",
     github: "https://github.com/username/portfolio",
     },
